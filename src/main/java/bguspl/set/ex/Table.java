b2a116dc61e77a634969a7279be109cfad4fb0df
lifeOnHack <ilayl9991@gmail.com>
2022-12-16 17:00:00 +0200

    upload table rebuild

--- src/main/java/bguspl/set/ex/Table.java
+++ src/main/java/bguspl/set/ex/Table.java
@@ -28,6 +28,9 @@ public class Table {
      * Mapping between a card and the slot it is in (null if none).
      */
     protected final Integer[] cardToSlot; // slot per card (if any)
+    private Integer[][] playersSets;
+    private final int NOT_PLACED = -1;
+    private final int MAX_TOKENS = 3;
 
     /**
      * Constructor for testing.
@@ -43,6 +46,12 @@ public class Table {
         this.env = env;
         this.slotToCard = slotToCard;
         this.cardToSlot = cardToSlot;
+        this.playersSets = new Integer[env.config.players][3];
+
+    }
+
+    public Integer[] getSTC() {
+        return slotToCard;
     }
 
     /**
@@ -51,7 +60,6 @@ public class Table {
      * @param env - the game environment objects.
      */
     public Table(Env env) {
-
         this(env, new Integer[env.config.tableSize], new Integer[env.config.deckSize]);
     }
 
@@ -100,8 +108,7 @@ public class Table {
 
         cardToSlot[card] = slot;
         slotToCard[slot] = card;
-
-        // TODO implement
+        env.ui.placeCard(card, slot);
     }
 
     /**
@@ -115,8 +122,13 @@ public class Table {
         } catch (InterruptedException ignored) {
         }
 
-        // TODO implement
+        cardToSlot[slotToCard[slot]] = null;
         slotToCard[slot] = null;
+        env.ui.removeCard(slot);
+    }
+
+    public void removeByCard(int card) {
+        removeCard(cardToSlot[card]);
     }
 
     /**
@@ -125,8 +137,18 @@ public class Table {
      * @param player - the player the token belongs to.
      * @param slot   - the slot on which to place the token.
      */
-    public void placeToken(int player, int slot) {
-        // TODO implement
+    public int placeToken(int pId, int slot) {
+        Integer[] pTokens = playersSets[pId];
+        for (int i = 0; i < MAX_TOKENS; i++) {
+            if (NOT_PLACED == pTokens[i]) {
+                synchronized (pTokens) {
+                    pTokens[i] = slot;
+                }
+                env.ui.placeToken(pId, slot);
+                return 1;
+            }
+        }
+        return 0;
     }
 
     /**
@@ -136,8 +158,87 @@ public class Table {
      * @param slot   - the slot from which to remove the token.
      * @return - true iff a token was successfully removed.
      */
-    public boolean removeToken(int player, int slot) {
-        // TODO implement
+    public boolean removeToken(int pId, int slot) {
+        Integer[] pTokens = playersSets[pId];
+        for (int i = 0; i < MAX_TOKENS; i++) {
+            if (slot == pTokens[i]) {
+                synchronized (pTokens) {
+                    pTokens[i] = NOT_PLACED;
+                }
+                env.ui.removeToken(pId, slot);
+                return true;
+            }
+        }
         return false;
+    }
+
+    /*
+     * deciding if to place or remove a token
+     */
+    public int setTokIfNeed(int pId, int slot) {
+        if (removeToken(pId, slot)) {
+            return -1;
+        }
+        return placeToken(pId, slot);
+    }
+
+    /*
+     * clear players tokens
+     */
+    public void reset() {
+        for (int i = 0; i < playersSets.length; i++) {
+            resetPlayer(playersSets[i], i);
+        }
+    }
+
+    private void resetPlayer(Integer[] pTokens, int id) {
+        synchronized (pTokens) {
+            for (int j = 0; j < MAX_TOKENS; j++) {
+                if (pTokens[j] != null && pTokens[j] != NOT_PLACED) {
+                    env.ui.removeToken(id, pTokens[j]);
+                }
+                pTokens[j] = NOT_PLACED;
+            }
+        }
+    }
+
+    public void resetPlayer(int pId) {
+        resetPlayer(playersSets[pId], pId);
+    }
+
+    public Integer[] getPlyrTok(int pId) {
+        return playersSets[pId];
+    }
+
+    public void removeAtPoint(int s1, int s2, int s3, Player[] players, LinkedList<Integer> requests) {
+        for (int i = 0; i < playersSets.length; i++) {
+            for (int j = 0; j < playersSets[i].length; j++) {
+                if (playersSets[i][j] == cardToSlot[s1]) {
+                    if (removeToken(i, cardToSlot[s1])) {
+                        players[i].tokenGotRemoved();
+                        rmvReq(players[i], requests);
+                    }
+                } else if (playersSets[i][j] == cardToSlot[s2]) {
+                    if (removeToken(i, cardToSlot[s2])) {
+                        players[i].tokenGotRemoved();
+                        rmvReq(players[i], requests);
+                    }
+                } else if (playersSets[i][j] == cardToSlot[s2]) {
+                    if (removeToken(i, cardToSlot[s3])) {
+                        players[i].tokenGotRemoved();
+                        rmvReq(players[i], requests);
+                    }
+                }
+            }
+        }
+    }
+
+    private void rmvReq(Player p, LinkedList<Integer> requests) {
+        synchronized (requests) {
+            if (requests.contains(p.id)) {
+                requests.remove(p.id);
+                p.myState.wakeup(p);
+            }
+        }
     }
 }
