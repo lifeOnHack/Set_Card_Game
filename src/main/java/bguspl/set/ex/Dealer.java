c1c82f5aa3c04209d34d36a181de471a9ffbd8d9
lifeOnHack <ilayl9991@gmail.com>
2022-12-18 13:15:41 +0200

    .

--- src/main/java/bguspl/set/ex/Dealer.java
+++ src/main/java/bguspl/set/ex/Dealer.java
@@ -76,15 +76,13 @@ public class Dealer implements Runnable {
         System.out.printf("Info: Thread %s starting.%n", Thread.currentThread().getName());
         initPlyrsThread();
         boolean s = true;
-        /*
-         * for quick testing
-         * for (int index = 0; index < deck.size(); index++) {
-         * if (index > 20) {
-         * deck.remove(index);
-         * index--;
-         * }
-         * }
-         */
+        // for quick testing
+        for (int index = 0; index < deck.size(); index++) {
+            if (index > 20) {
+                deck.remove(index);
+                index--;
+            }
+        }
         shuffleNReset();
         while (!shouldFinish()) {
             placeCardsOnTable();
